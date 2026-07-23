984071d763eab1fa8acdc3f3d902f8041601008e
NiXxihn <104733995+ArthurBarcelos1@users.noreply.github.com>
2026-07-23 17:08:14 -0300

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "MyCloset-v1.0.0";
+
+const urlsToCache = [
+    "./",
+    "./index.html",
+    "./catalogo.html",
+    "./designs.html",
+    "./liked.html",
+    "./login.html",
+
+    "./css/styles.css",
+
+    "./js/app.js",
+
+    "./images/icons/icon-192.png",
+    "./images/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
